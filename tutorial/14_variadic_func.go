328cbc6de8b49ff465fff33c2c95a4be4c28b1ba
jinheonyoon <yjinheon@gmail.com>
2021-12-29 21:48:21 +0900

    gorun

---
+++ tutorial/14_variadic_func.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// variadic function은 인자를 여러개 받을 수 있는 함수를 의미한다.
+//
+
+
+func sum(nums...int) { // int 여러개를 인자로 받음
+
+  fmt.Println(nums," ")
+  total := 0 // `:=` is a declaration , whreas `=` is an assignment
+
+  for _,num := range nums {
+    total += num
+  }
+  fmt.Println(total)
+  
+}
+
+func main() {
+  
+  sum(1,2)
+  sum(1,2,3)
+
+  nums := []int{1,2,3,4}
+  sum(nums...)
+}
